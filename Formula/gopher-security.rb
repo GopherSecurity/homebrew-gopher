03e37dbc422944ed3db8411795982a1139de8e43
Divyansh Ingle <divyanshingle@gmail.com>
2024-09-11 18:55:26 +0530

    Update formula

--- Formula/gopher-security.rb
+++ Formula/gopher-security.rb
@@ -1,15 +1,25 @@
 class GopherSecurity < Formula
-    desc "Description of your app"
+    desc "Gopher Security application"
     homepage "https://www.gopher.security"
     url "https://s3.amazonaws.com/public.gopher.security/apps/release/darwin/GopherSecurity-darwin-0.0.2-15-amd64-release.pkg"
     sha256 "e0e640d5f8077f6af8026ee7bef9fc90df8b6aabc78164f3713eae70532dc213"
+    version "0.0.2-15"
   
     def install
-      system "installer", "-pkg", "GopherSecurity-darwin-0.0.2-16-amd64-release.pkg", "-target", "/"
+      pkg_name = "GopherSecurity-darwin-0.0.2-15-amd64-release.pkg"
+      system "/usr/sbin/installer", "-pkg", "#{cached_download}", "-target", "/"
     end
-
-    def uninstall
-        system "./Applications/GopherSecurity/uninstallAll"
-      end
-  end
   
+    def uninstall
+      system "sudo", "/Applications/GopherSecurity/uninstallAll"
+    end
+  
+    def caveats
+      <<~EOS
+        GopherSecurity has been installed.
+        To uninstall, run:
+          brew uninstall gopher-security
+        You may need to restart your computer for all changes to take effect.
+      EOS
+    end
+  end
