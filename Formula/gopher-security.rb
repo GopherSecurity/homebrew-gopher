4131685247ae01b55806d8be8dd61f7e7838f3e6
Divyansh Ingle <divyanshingle@gmail.com>
2024-09-11 19:46:18 +0530

    fix issue

--- Formula/gopher-security.rb
+++ Formula/gopher-security.rb
@@ -1,7 +1,7 @@
 class GopherSecurity < Formula
     desc "Gopher Security application"
     homepage "https://www.gopher.security"
-    url "https://s3.amazonaws.com/public.gopher.security/apps/release/darwin/GopherSecurity-darwin-0.0.2-15-amd64-debug.pkg"
+    url "https://s3.amazonaws.com/public.gopher.security/apps/release/darwin/GopherSecurity-darwin-0.0.2-15-amd64-release.pkg"
     sha256 "e0e640d5f8077f6af8026ee7bef9fc90df8b6aabc78164f3713eae70532dc213"
     version "0.0.2-15"
   
