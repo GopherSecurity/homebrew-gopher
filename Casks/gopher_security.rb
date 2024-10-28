5c546d0fe9d500033b927b9cdfea4586e43d97c0
Divyansh Ingle <divyanshingle@gmail.com>
2024-10-28 19:21:38 +0530

    Update latest build in cask config

--- Casks/gopher_security.rb
+++ Casks/gopher_security.rb
@@ -1,8 +1,8 @@
 cask "gopher_security" do
-    version "0.0.2-15"
-    sha256 "e0e640d5f8077f6af8026ee7bef9fc90df8b6aabc78164f3713eae70532dc213"
+    version "0.0.2-31"
+    sha256 "9534ec05b689d22ef0a66c113484f4a4e75011386f975b2da3dd491fd08e3539"
   
-    url "https://s3.amazonaws.com/public.gopher.security/apps/release/darwin/GopherSecurity-darwin-#{version}-amd64-release.pkg"
+    url "https://download.gopher.security/apps/release/darwin/GopherSecurity-darwin-#{version}-amd64-release.pkg"
     name "Gopher Security"
     desc "Gopher Security application"
     homepage "https://www.gopher.security"
